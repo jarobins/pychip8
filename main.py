85549b3176badf401075341ea99d4395b2362a06
Jake Robinson <jarobins139@gmail.com>
2019-08-01 22:23:18 -0400

    main.py: Version and comments
    Added in some comments and version numbers for the code. Dont think I
    will be changing much to the code over time, but nice to keep the
    habit.

---
+++ main.py
@@ -0,0 +1,19 @@
+######
+# PyChip8
+
+# Author: Jake Robinson
+# Date  : 08/01/2019
+
+# Running through a list of CPUs to emulate. Trying a few things to learn
+# a process to do these emulations easy.
+
+# Verison:
+# 0.0 - First commit, adding comments and other starting things
+min_ver, maj_ver = 0,0
+
+
+
+
+if __name__ == "__main__":
+    print("Python Chip 8 Emulator")
+    print("Verison: {}.{}".format(maj_ver, min_ver))
