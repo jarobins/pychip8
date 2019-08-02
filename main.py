fbc46514a7bb2a082c4fa4165b849602f424ebcb
Jake Robinson <jarobins139@gmail.com>
2019-08-01 22:57:31 -0400

    main.py: CPU class and RAM
    Added in the CPU class with some placeholders and
    implemented loading the RAM and reading the data.

--- main.py
+++ main.py
@@ -11,8 +11,31 @@
 # 0.0 - First commit, adding comments and other starting things
 min_ver, maj_ver = 0,0
 
+RAM = []  # Im not sure if I like the RAM being a list
+
+class CPU():
+    ''' Handles the opcodes and the register values '''
+    def __init__(self):
+        # Registers
+        pass
+
+    def decode(self, opcode):
+        # Just going to be a large if statment
+        # There might be a way to do this with
+        # dictionaries, but this will work for now
+        pass
 
 
+with open('./pong.ch8', 'rb') as f:
+    raw_data = f.read()
+
+# load the RAM
+for item in [(x,y) for x,y in zip(raw_data[::2], raw_data[1::2])]:
+    RAM.append("{:02X}{:02X}".format(ord(item[0]), ord(item[1])))
+
+
+
+    
 
 if __name__ == "__main__":
     print("Python Chip 8 Emulator")
